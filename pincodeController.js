35611117e463d79b748979399cb595808a2bf93b
Alok Babu <alok.707@gmail.com>
2015-08-11 00:10:32 +0530

    Update pincodeController.js

--- pincodeController.js
+++ pincodeController.js
@@ -17,7 +17,7 @@ app.controller('pincodeController', ['$scope', '$http', 'usSpinnerService', func
 			$scope.startSpin();
 			var api = "https://data.gov.in/api/datastore/resource.json?"
 						+"resource_id=0a076478-3fd3-4e2c-b2d2-581876f56d77"
-						+"&api-key=05eaa3254126155108dcf6e1557058e4"
+						+"&api-key=[YOUR-API_KEY]]"
 						+"&limit=100"
 						+"&filters[pincode]="+pincode
 						//+"&filters[officename]="+$scope.pincode
